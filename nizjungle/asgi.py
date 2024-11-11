f65910d4678d1052766246d20768003ff989c79a
dataSha95 <shahedpremmium@gmail.com>
2024-11-11 03:22:12 +0000

    Added basic home page design

---
+++ nizjungle/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for nizjungle project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nizjungle.settings")
+
+application = get_asgi_application()
