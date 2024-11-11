0cf37dbe925b71665ed7875f800d3ca7bb3cf849
dataSha95 <shahedpremmium@gmail.com>
2024-11-11 03:58:47 +0000

    Added basic home page design

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('programs/', views.programs, name='programs'),
+    path('community/', views.community, name='community'),
 ]
--- home/views.py
+++ home/views.py
@@ -2,3 +2,13 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home/index.html')
+
+def about(request):
+    return render(request, 'home/about.html')
+
+def programs(request):
+    return render(request, 'home/programs.html')
+
+
+def community(request):
+    return render(request, 'home/community.html') 
